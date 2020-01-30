09c8d0151f75119566a6bfd5729a6e70d1a136bf
sgigli <simon.luciano@gigli.net>
2020-01-30 12:17:51 -0500

    Made 'name' field unique in chatroom model

--- app/models/chatroom.js
+++ app/models/chatroom.js
@@ -4,7 +4,8 @@ const messageSchema = require('./message')
 const chatroomSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
